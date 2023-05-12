6a5de642605d1620737e3ccb6702e8db2d7ea142
Snorps <znellsnorps@outlook.com>
2023-05-12 13:14:08 +0100

    Tried some more stuff.

--- src/main/java/net/snorps/endofmending/Config.java
+++ src/main/java/net/snorps/endofmending/Config.java
@@ -2,49 +2,43 @@ package net.snorps.endofmending;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import org.apache.commons.lang3.tuple.Pair;
-
+@Mod.EventBusSubscriber(modid = "endofmending", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
-    public static final Config COMMON;
+    public static final CommonConfig COMMON;
     public static final ForgeConfigSpec SPEC_COMMON;
     static {
-        final Pair<Config, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Config::new);
+        final Pair<CommonConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(CommonConfig::new);
         COMMON = specPair.getLeft();
         SPEC_COMMON = specPair.getRight();
     }
 
-    public static double DURABILITY_MULT;
+    public static double durabilityMult;
 
     @SubscribeEvent
     public static void onModConfigEvent(final ModConfigEvent.Loading configEvent) {
         if (configEvent.getConfig().getSpec() == Config.SPEC_COMMON) {
-            DURABILITY_MULT = COMMON.durabilityMult.get();
+            durabilityMult = COMMON.durabilityMult.get();
+            int a = 3;
         }
     }
 
-    protected final ForgeConfigSpec.ConfigValue<Double> durabilityMult;
+    public static class CommonConfig  {
+        public final ForgeConfigSpec.DoubleValue durabilityMult;
 
-    public Config(final ForgeConfigSpec.Builder BUILDER) {
-        BUILDER
-                .comment(
-                        """
-                                
-                          End of Mending Config - Common
-                          
-""")
-                .push("General");
-
-        durabilityMult = BUILDER
-                .comment(
-                        """
-                                The multiplier to apply to the durability of all repairable items. e.g 2.0 will double all durability
-                                1 item will still repair half of an items durability, so this decreases the amount of resources a player will
-                                end up spending repairing their tools, weapons, and armour.
-                                Default: 2.0""".indent(1))
-                .worldRestart()
-                .define("Durability Multiplier", 2.0);
-
-        BUILDER.pop();
+        CommonConfig(ForgeConfigSpec.Builder builder) {
+            builder.push("durability multiply");
+            durabilityMult = builder.translation("endofmending" + ".config.durabilityMultiplier")
+                    .comment(
+                            """
+                                    The multiplier to apply to the durability of all repairable items. e.g 2.0 will double all durability
+                                    1 item will still repair half of an items durability, so this decreases the amount of resources a player will
+                                    end up spending repairing their tools, weapons, and armour.
+                                    Default: 2.0""".indent(1))
+                    .defineInRange("Durability Multiplier", 2.0D, 0.0D, 100.0D);
+            builder.pop();
+        }
     }
 }
--- src/main/java/net/snorps/endofmending/mixin/ArmorMaterialsMixin.java
+++ src/main/java/net/snorps/endofmending/mixin/ArmorMaterialsMixin.java
@@ -3,14 +3,15 @@ package net.snorps.endofmending.mixin;
 import net.minecraft.world.item.ArmorMaterials;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.snorps.endofmending.Config;
 
 @Mixin(ArmorMaterials.class)
 public class ArmorMaterialsMixin {
-    @ModifyVariable(method = "<init>", require = 0, argsOnly = true, at = @At(value = "HEAD"), index = 4)
+    /*@ModifyVariable(method = "<init>", require = 0, argsOnly = true, at = @At(value = "HEAD"), index = 4)
     private static int replaceDurabilityMult(int defaultValue) {
-        return (int)(defaultValue * Config.COMMON.DURABILITY_MULT);
-    }
+        return (int)(defaultValue * Config.durabilityMult);
+    }*/
 }
